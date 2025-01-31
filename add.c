79f82dbc52dfbf5686b098ba07083734ae1cc785
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 12:49:51 +0545

    Add files via upload

---
+++ add.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+int arr[2][2];
+int i,j;
+
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+        scanf("%d\t", &arr[i][j]);
+    }
+}
+int sum = 0;
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+    sum += arr[i][j];
+}
+}
+printf("The sum is: %d", sum);
+return 0;
+}
