a4ee3357b3b9018a046b32beed7b137c5f45382a
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 14:18:17 +0545

    Add files via upload

---
+++ storetran.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+    int r,c;
+    printf("Enter the no. of rows: ");
+    scanf("%d", &r);
+    printf("Enter the no. of columns: ");
+    scanf("%d", &c);
+
+
+    int arr[r][c];
+    int brr[c][r];
+    int i,j;
+
+for (i=0; i<r; i++) {
+    for (j=0; j<c; j++) {
+    scanf("%d", &arr[i][j]);
+    }
+}
+
+for (i=0; i<c; i++) {
+    for (j=0; j<r; j++) {
+        brr[i][j] = arr[j][i];
+    }
+}
+for (i=0; i<c; i++) {
+    for (j=0; j<r; j++) {
+        printf("%d\t", brr[i][j]);
+    }
+    printf("\n");
+}
+return 0;
+}
