cf612009f4e631d1bd109c8f39f653705c87c05d
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 14:07:32 +0545

    Add files via upload

---
+++ transpose.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+    int arr[2][3]= {2,44,53,5,78,89};
+    int i,j;
+
+for (i=0; i<3; i++) {
+    for (j=0; j<2; j++) {
+        printf("%d\t", arr[j][i]);
+    }
+    printf("\n");
+}
+return 0;
+}
