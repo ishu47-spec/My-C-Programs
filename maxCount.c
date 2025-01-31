fd8dd173cac7e3d5e9cd878bca8e158bcc524175
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 13:55:31 +0545

    Add files via upload

---
+++ maxCount.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+int arr[3][3] = {{1,1,1}, {0,1,1}, {1,0,0}};
+int i,j;
+int maxcount=0;
+
+for (i=0; i<3; i++) {
+        int count = 0;
+    for (j=0; j<3; j++) {
+        if (arr[i][j] == 1) {
+            count++;
+        }
+        if (maxcount < count) {
+            maxcount = count;
+        }
+    }
+}
+printf("%d", maxcount);
+return 0;
+}
