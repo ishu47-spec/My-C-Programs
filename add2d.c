43446a75d4a2aeb869bdd4c108d2c320cf2b5c4f
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 12:41:01 +0545

    Add files via upload

---
+++ add2d.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main() {
+int arr[2][2];
+int brr[2][2];
+int result[2][2];
+int i,j;
+
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+        scanf("%d", &arr[i][j]);
+    }
+}
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+        scanf("%d", &brr[i][j]);
+    }
+}
+printf("\n");
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+    result[i][j] = arr[i][j] + brr[i][j];
+    }
+}
+for (i=0; i<2; i++) {
+    for (j=0; j<2; j++) {
+ printf("%d\t", result[i][j]);
+    }
+printf("\n");
+}
+return 0;
+}
