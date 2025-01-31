91184c205d7e3e021031093b689b2bf25e4e3e7c
ishu47-spec <ishankadal338@gmail.com>
2025-01-31 15:33:20 +0545

    Add files via upload

---
+++ structure2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct book {
+int bookID;
+char title[150];
+float price;
+};
+
+int main() {
+struct book b[5];
+int i;
+
+for (i=0; i<5; i++) {
+printf("\nEnter book ID: ");
+scanf("%d", &b[i].bookID);
+
+printf("\nEnter book title: ");
+fflush(stdin);
+gets(b[i].title);
+
+printf("\nEnter book price: ");
+scanf("%f", &b[i].price);
+}
+
+for (i=0; i<5; i++) {
+printf("\nBookID = %d\tBook Title = %s\t Book price = Rs.%.2f", b[i].bookID, b[i].title, b[i].price);
+}
+return 0;
+}
