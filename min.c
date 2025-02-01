943ab476ad2bdddb58fa5873217b6cc011f9c001
ishu47-spec <ishankadal338@gmail.com>
2025-02-01 23:01:20 +0545

    Added comments.

--- min.c
+++ min.c
@@ -1,7 +1,7 @@
 /*This program prints the minimum of 2 numbers using function.*/
 #include<stdio.h>
 
-int minimum(int x, int y) {
+int minimum(int x, int y) {          //int x and int y are arguments in place of int a and int b from main function..
 return (x<y)? x:y;
 }
 
