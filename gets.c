ebab6dbdc27ef1fbc9b94a99658ca803fdd98339
ishu47-spec <ishankadal338@gmail.com>
2025-02-02 07:53:40 +0545

    Added comments.

--- gets.c
+++ gets.c
@@ -1,3 +1,4 @@
+/*This program takes input as string from user and displays it.*/
 #include<stdio.h>
 #include<string.h>
 
@@ -5,6 +6,6 @@ int main() {
 char str[100];
 printf("Enter your info: ");
 gets(str);
-printf("Your info is: %s", str);
+printf("Your info is: %s", str);         // puts(str) will print the string only which the user inputs.
 return 0;
 }
