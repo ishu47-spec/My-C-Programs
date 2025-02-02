1d465c7fdeb58307cddf41a2d8f44fff534598d9
ishu47-spec <ishankadal338@gmail.com>
2025-02-02 07:05:35 +0545

    Added comments.

--- string.c
+++ string.c
@@ -4,7 +4,7 @@
 int main() {
 char str[] = "Hello I'm a coder!";
 int i=0;
-while(str[i]!='\0'){
+while(str[i]!='\0'){             //Here, /0 is a default value and is always present at the end of the string.
     printf("%c", str[i]);
     i++;
 }
