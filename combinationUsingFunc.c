08a086087a6342b5ec933e5805d547670afea4f9
ishu47-spec <ishankadal338@gmail.com>
2025-02-04 08:36:35 +0545

    Add files via upload

---
+++ combinationUsingFunc.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int factorial(x) {
+int fact=1, i;
+for (i=1; i<=x; i++) {
+fact = fact * i;
+}
+return fact;
+}
+
+
+int main() {
+int n,r;
+printf("Enter the value of n and r: ");
+scanf("%d%d", &n, &r);
+
+int nfact=factorial(n);
+int rfact=factorial(r);
+int nrfact=factorial(n-r);
+
+int ncr = nfact/(rfact*nrfact);
+printf("%d", ncr);
+return 0;
+
+
+}
